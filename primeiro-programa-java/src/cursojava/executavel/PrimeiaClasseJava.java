889324d9a9ba0a3906c4995154404a33295c876a
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-11-08 01:18:01 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiaClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiaClasseJava {
+	
+	/* Main é um metodo auto executavel em Java */
+	public static void main(String[] args) {
+		
+		/* Escreve texto no console */
+		System.out.println("Meu primeiro programa em java - do Curso Formação Java Web");
+		System.out.println("Java é Orientado a Objetos");
+	}
+	
+}
