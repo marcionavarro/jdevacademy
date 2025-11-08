58178256aadbc07e0fb39d0d28d179eeb60ce7ac
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-11-08 11:31:47 -0300

    adicionando data nascimento 2

--- primeiro-programa-java/src/curso/java/marcio/Pessoa.java
+++ primeiro-programa-java/src/curso/java/marcio/Pessoa.java
@@ -8,6 +8,7 @@ public class Pessoa {
 	private String sobrenome;
 	private int idade;
 	private Date dataNascimento;
+	private Date dataNascimento2;
 	
 	public int getIdade() {
 		return idade;
@@ -39,6 +40,14 @@ public class Pessoa {
 
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
+	}
+
+	public Date getDataNascimento2() {
+		return dataNascimento2;
+	}
+
+	public void setDataNascimento2(Date dataNascimento2) {
+		this.dataNascimento2 = dataNascimento2;
 	}
 
 	
