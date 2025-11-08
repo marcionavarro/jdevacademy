825a544f3358d7405f2ceeb002be154b4eaf1d9f
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-11-08 13:55:36 -0300

    Finalizando o modulo 8

--- primeiro-programa-java/src/cursojava/executavel/PrimeiaClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiaClasseJava.java
@@ -1,13 +1,36 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiaClasseJava {
-	
+
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
+
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
 		
-		/* Escreve texto no console */
-		System.out.println("Meu primeiro programa em java - do Curso Formação Java Web");
-		System.out.println("Java é Orientado a Objetos");
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota1);
+		double dNota3 = Double.parseDouble(nota1);
+		double dNota4 = Double.parseDouble(nota1);
+		
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+		
+		/* Média para aprovação é 70 */
+		
+		if(media >= 50) {
+			if(media >= 70) {
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com média de: " + media);
+			} else {
+				JOptionPane.showMessageDialog(null, "Aluno está de recuperação com média de: " + media);
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Aluno está reprovado com média de: " + media);
+		}
+
 	}
-	
+
 }
